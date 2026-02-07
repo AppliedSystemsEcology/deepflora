246c66e5ce2cbcc395d6b78b52e494c461966cc0
Kevin <likevin@umich.edu>
2026-02-07 13:05:29 -0500

    download images with warnings

--- scripts/download_naip_pa.R
+++ scripts/download_naip_pa.R
@@ -9,6 +9,11 @@ index_html <- "https://naipeuwest.blob.core.windows.net/naip/v002/pa/2017/pa_100
 state_index_links <- azure_from_index(index_html)
 state_outdir <- basename(dirname(index_html))
 
+# logging
+zz <- file("all.Rout", open="wt")
+sink(zz, type="message", append = TRUE)
+sink(zz, type = "output", append = TRUE)
+
 # loop over each index file
 for (i in seq_along(state_index_links)){
   index.i <- state_index_links[i]
@@ -18,10 +23,21 @@ for (i in seq_along(state_index_links)){
   if(!dir.exists(outputdir.i)){dir.create(outputdir.i, recursive = TRUE)}
   # get the data files in index.i
   files.i <- azure_from_index(index.i)
+  cat("trying", basename(files.i),"\n")
   # build output filepaths
   outputfiles.i <- file.path(outputdir.i,basename(files.i))
+  # check for existing files
+  exist.outputfiles <- file.exists(outputfiles.i)
+  # update list for files to download and destinations
+  files.i <- files.i[!exist.outputfiles]
+  outputfiles.i <- outputfiles.i[!exist.outputfiles]
   # download to file structure
-  download.file(files.i,
-                destfile = outputfiles.i,
-                method="libcurl")
+  tryCatch(
+    download.file(files.i,
+                  destfile = outputfiles.i,
+                  method="libcurl"),
+    error = function(e) message(e)
+  )
 }
+sink(type="message")
+sink(type = "output")
