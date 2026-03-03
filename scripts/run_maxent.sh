d1cb267e3d5748661b5b3c224870fd348aba6c1f
Kevin <likevin@umich.edu>
2026-03-03 10:29:25 -0500

    sdm scripts

---
+++ scripts/run_maxent.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --job-name=deepflora_maxent
+#SBATCH --account=hlc30_p100_default
+#SBATCH --partition=sla-prio
+#SBATCH --gres=gpu:p100:1
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=26
+#SBATCH --mem=224G
+#SBATCH --time=48:00:00
+#SBATCH --output=maxent_%j.out
+#SBATCH --error=maxent_%j.err
+
+module load anaconda
+source activate deepflora_r
+
+# uniform test / train split
+echo "Processing uniform train-test random forest"
+Rscript src/deepbiosphere/src/deepbiosphere/scripts/Maxent_RF_bioclim.R --band unif_train_test --sdm rf --ncpu 26
+
+echo "Processing uniform train-test maxent"
+Rscript src/deepbiosphere/src/deepbiosphere/scripts/Maxent_RF_bioclim.R --dset_name  --band unif_train_test --sdm maxent --ncpu 26
+
+# spatial cross-validation band example
+for band in $(seq 0 11); do
+  echo "Processing band ${band} of 11 for random forest..."
+  Rscript src/deepbiosphere/src/deepbiosphere/scripts/Maxent_RF_bioclim.R --band band_${band} --sdm rf --ncpu 26
+done
+
+for band in $(seq 0 11); do
+  echo "Processing band ${band} of 11 for maxent..."
+  Rscript src/deepbiosphere/src/deepbiosphere/scripts/Maxent_RF_bioclim.R --band band_${band} --sdm maxent --ncpu 26
+done
