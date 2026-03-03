1ae8a0ea950e082dea06c85649f4f68fcc0dc704
Kevin <likevin@umich.edu>
2026-03-03 11:32:23 -0500

    run_maxent.sh add indicators for failure

--- scripts/run_maxent.sh
+++ scripts/run_maxent.sh
@@ -17,17 +17,30 @@ source activate deepflora_r
 # uniform test / train split
 echo "Processing uniform train-test random forest"
 Rscript src/deepbiosphere/src/deepbiosphere/Maxent_RF_bioclim.R --band unif_train_test --sdm rf --ncpu 26
+if [ $? -ne 0 ]; then
+    echo "ERROR: uniform train-test random forest failed"
+fi
 
 echo "Processing uniform train-test maxent"
 Rscript src/deepbiosphere/src/deepbiosphere/Maxent_RF_bioclim.R --dset_name  --band unif_train_test --sdm maxent --ncpu 26
+if [ $? -ne 0 ]; then
+    echo "ERROR: uniform train-test maxent failed"
+fi
+
 
 # spatial cross-validation band example
 for band in $(seq 0 11); do
   echo "Processing band ${band} of 11 for random forest..."
   Rscript src/deepbiosphere/src/deepbiosphere/Maxent_RF_bioclim.R --band band_${band} --sdm rf --ncpu 26
+  if [ $? -ne 0 ]; then
+    echo "ERROR: band ${band} random forest failed"
+  fi
 done
 
 for band in $(seq 0 11); do
   echo "Processing band ${band} of 11 for maxent..."
   Rscript src/deepbiosphere/src/deepbiosphere/Maxent_RF_bioclim.R --band band_${band} --sdm maxent --ncpu 26
+  if [ $? -ne 0 ]; then
+    echo "ERROR: band ${band} maxent failed"
+  fi
 done
