2ae82c0af9def0a272c825274c1dde37d5196bcd
Kevin <likevin@umich.edu>
2026-03-10 12:23:39 -0400

    sbatch scripts

--- scripts/run_maxent.sh
+++ scripts/run_maxent.sh
@@ -8,18 +8,11 @@
 #SBATCH --cpus-per-task=26
 #SBATCH --mem=224G
 #SBATCH --time=48:00:00
-#SBATCH --output=maxent_%j.out
-#SBATCH --error=maxent_%j.err
+#SBATCH --output=sdm_maxent_%j.out
+#SBATCH --error=sdm_maxent_%j.err
 
 module load anaconda
 source activate deepflora_r
-
-# uniform test / train split
-echo "Processing uniform train-test random forest"
-Rscript src/deepbiosphere/src/deepbiosphere/Maxent_RF_bioclim.R --dset_name plants_pa --band unif_train_test --sdm rf --ncpu 26
-if [ $? -ne 0 ]; then
-    echo "ERROR: uniform train-test random forest failed"
-fi
 
 echo "Processing uniform train-test maxent"
 Rscript src/deepbiosphere/src/deepbiosphere/Maxent_RF_bioclim.R --dset_name plants_pa --band unif_train_test --sdm maxent --ncpu 26
@@ -27,15 +20,6 @@ if [ $? -ne 0 ]; then
     echo "ERROR: uniform train-test maxent failed"
 fi
 
-
-# spatial cross-validation band example
-for band in $(seq 0 11); do
-  echo "Processing band ${band} of 11 for random forest..."
-  Rscript src/deepbiosphere/src/deepbiosphere/Maxent_RF_bioclim.R --dset_name plants_pa --band band_${band} --sdm rf --ncpu 26
-  if [ $? -ne 0 ]; then
-    echo "ERROR: band ${band} random forest failed"
-  fi
-done
 
 for band in $(seq 0 11); do
   echo "Processing band ${band} of 11 for maxent..."
---
+++ scripts/run_rf.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=deepflora_rf
+#SBATCH --account=hlc30_p100_default
+#SBATCH --partition=sla-prio
+#SBATCH --gres=gpu:p100:1
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=26
+#SBATCH --mem=224G
+#SBATCH --time=48:00:00
+#SBATCH --output=sdm_rf_%j.out
+#SBATCH --error=sdm_rf_%j.err
+
+module load anaconda
+source activate deepflora_r
+
+# uniform test / train split
+echo "Processing uniform train-test random forest"
+Rscript src/deepbiosphere/src/deepbiosphere/Maxent_RF_bioclim.R --dset_name plants_pa --band unif_train_test --sdm rf --ncpu 26 --remakebkgd
+if [ $? -ne 0 ]; then
+    echo "ERROR: uniform train-test random forest failed"
+fi
+
+# spatial cross-validation
+for band in $(seq 0 11); do
+  echo "Processing band ${band} of 11 for random forest..."
+  Rscript src/deepbiosphere/src/deepbiosphere/Maxent_RF_bioclim.R --dset_name plants_pa --band band_${band} --sdm rf --ncpu 26 --remakebkgd
+  if [ $? -ne 0 ]; then
+    echo "ERROR: band ${band} random forest failed"
+  fi
+done
