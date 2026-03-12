4b2d9fff95944b69a00b35555e29db70c874c9e2
Kevin <likevin@umich.edu>
2026-03-12 10:38:20 -0400

    spatial cv deepbiosphere sbatch

--- scripts/run_rf.sh
+++ scripts/run_rf.sh
@@ -23,7 +23,7 @@ fi
 
 # spatial cross-validation
 for band in $(seq 0 9); do
-  echo "Processing band ${band} of 11 for random forest..."
+  echo "Processing band ${band} of 9 for random forest..."
   Rscript src/deepbiosphere/src/deepbiosphere/Maxent_RF_bioclim.R --dset_name plants_pa --band band_${band} --sdm rf --ncpu 26 --remakebkgd
   if [ $? -ne 0 ]; then
     echo "ERROR: band ${band} random forest failed"
