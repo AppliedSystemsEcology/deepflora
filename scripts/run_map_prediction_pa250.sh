ef877f9529f74174b701d005e8cac553a7b0cec1
Kevin <likevin@umich.edu>
2026-04-07 12:05:46 -0400

    map script

---
+++ scripts/run_map_prediction_pa250.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --job-name=deepflora_pa_250
+#SBATCH --account=hlc30_p100_default
+#SBATCH --partition=sla-prio
+#SBATCH --gres=gpu:p100:1
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=224G
+#SBATCH --time=48:00:00
+#SBATCH --output=pa250_%j.out
+#SBATCH --error=pa250_%j.err
+
+module load anaconda
+source activate deepflora
+
+python work/github/deepflora/scripts/predict_State_College.py
+
+python /storage/home/kbl5733/src/deepbiosphere/src/deepbiosphere/Make_Maps.py \
+  --shape_pth /storage/home/kbl5733/gstorage/data/deepflora/SHPFILES/states/pa.shp \
+  --parent_dir pa_250 \
+  --pred_year 2017 \
+  --state pa
+  --pred_types ALPHA \
+  --loss SAMPLE_AWARE_BCE \
+  --exp_id initial \
+  --band -1 \
+  --loss SAMPLE_AWARE_BCE \
+  --architecture DEEPBIOSPHERE \
+  --epoch 7 \
+  --batch_size 50 \
+  --device 0 \
+  --processes 8
