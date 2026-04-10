2dd0c03e64a15073277ac81518f5508d16e13e93
Kevin <likevin@umich.edu>
2026-04-10 12:08:00 -0400

    update website

--- data-raw/accuracy_metrics.R
+++ data-raw/accuracy_metrics.R
@@ -5,13 +5,13 @@ metricfiles <- list.files(metricsdir, full.names = TRUE)
 beeplants <- readRDS("data/beeplants.rds")
 
 overall <- lapply(metricfiles[grep("overall_results", metricfiles)], read.csv)
-names(overall) <- strsplit(basename(metricfiles[grep("overall_results", metricfiles)]), "_") |> sapply(\(x) unlist(x)[1])
+names(overall) <- strsplit(basename(metricfiles[grep("overall_results", metricfiles)]), "_") |> sapply(\(x) paste(unlist(x)[1:2],collapse="_"))
 
 perobs <- lapply(metricfiles[grep("observations", metricfiles)], read.csv)
-names(perobs) <- strsplit(basename(metricfiles[grep("observations", metricfiles)]), "_") |> sapply(\(x) unlist(x)[1])
+names(perobs) <- strsplit(basename(metricfiles[grep("observations", metricfiles)]), "_") |> sapply(\(x) paste(unlist(x)[1:2],collapse="_"))
 
 persp <- lapply(metricfiles[grep("species", metricfiles)], read.csv)
-names(persp) <- strsplit(basename(metricfiles[grep("species", metricfiles)]), "_") |> sapply(\(x) unlist(x)[1])
+names(persp) <- strsplit(basename(metricfiles[grep("species", metricfiles)]), "_") |> sapply(\(x) paste(unlist(x)[1:2],collapse="_"))
 
 make_persp_long <- function(x) {
   x |>
@@ -27,7 +27,7 @@ make_persp_long <- function(x) {
                          cols_remove = FALSE)
 }
 
-persp.l <- lapply(persp, make_persp_long)
+persp.l <- lapply(persp[c("bioclim_unif", "initial_db", "maxent_unif", "rf_unif", "tresnet_unif")], make_persp_long)
 
 # all bee floral resource sp (Grozinger + Fowler)
 # persp.fr <- persp.l %>%
@@ -45,7 +45,15 @@ persp.fr <- lapply(persp.l, \(x){
 # plotting df
 plot_persp <- list(full=bind_rows(persp.l, .id = "model"), floralres=bind_rows(persp.fr, .id = "model")) |>
   bind_rows(.id = "subset") |> dplyr::filter(support >= 1) |>
-  pivot_longer(species_top1:support, names_to = "metric")
+  pivot_longer(species_top1:support, names_to = "metric") |>
+  mutate(model = str_remove(model, "_[^.]*$"))
 
 dir.create("data/results/")
 write.csv(plot_persp, "data/results/per-sp-accuracy.csv", row.names = FALSE)
+
+
+## overall results
+overall.red <- lapply(overall, \(x) dplyr::select(x, where(~!all(is.na(.x)))))  # select only columns where not NaN
+overall_df <- bind_rows(overall.red, .id = "model")
+
+write.csv(overall_df, "data/results/overall-accuracy.csv", row.names = FALSE)
--- plots/R/uniform_train.R
+++ plots/R/uniform_train.R
@@ -1,42 +1,10 @@
-
 library(tidyverse)
 library(DT)
 library(rstatix)
 library(ggsignif)
 
 plot_persp <- read.csv("data/results/per-sp-accuracy.csv")
-
-model_labels <- c(
-  "initial" = "Deepbiosph.",
-  "bioclim" = "Bioclim MLP",
-  "tresnet" = "TResNet",
-  "maxent" = "Maxent",
-  "rf" = "Rand. Forest"
-)
-
-# Map your metric column values to display labels
-metric_labels <- c(
-  "calibrated_PRC_AUC" = "calibrated~AUC[PRC]",
-  "calibrated_ROC_AUC" = "calibrated~AUC[ROC]",
-  "f1_score"           = "F1~score",
-  "PRC_AUC"            = "AUC[PRC]",
-  "precision_score"    = "Precision~score",
-  "recall_score"       = "Recall~score",
-  "ROC_AUC"            = "AUC[ROC]",
-  "species_top1"       = "Top~'1'[spp]",
-  "species_top100"     = "Top~'100'[spp]",
-  "species_top30"      = "Top~'30'[spp]",
-  "species_top5"       = "Top~'5'[spp]",
-  "zero_one_accuracy"  = "Presence~accuracy"
-)
-
-all_labels <- append(model_labels, metric_labels)
-
-# Desired panel order (matches reading order in the figure)
-metric_order <- c(
-  "zero_one_accuracy", "calibrated_PRC_AUC", "calibrated_ROC_AUC",
-  "PRC_AUC", "ROC_AUC",  "precision_score",   "recall_score", "f1_score",
-  "species_top1", "species_top5","species_top30","species_top100")
+source("plots/R/plot_standards.R")
 
 # spacing in brackets
 setspacing <- Vectorize(\(x) switch(as.character(x), bioclim = 1, tresnet = 1.1, maxent = 1.2, rf = 1.3))
