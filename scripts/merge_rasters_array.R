77a71fcfb4b07367e16963c0588e135b0438306c
Kevin <likevin@umich.edu>
2026-04-17 14:42:03 -0400

    back to sprc

--- scripts/merge_rasters_array.R
+++ scripts/merge_rasters_array.R
@@ -10,13 +10,11 @@ array_tifs <- list.files(array_dir, pattern = "*raw.tif", full.names = TRUE)
 
 terraOptions(memfrac = 0.6, tempdir = "/scratch/kbl5733/tmp", threads = 8)
 
-out.vrt <- paste0("/storage/home/kbl5733/gstorage/data/deepflora/maps/vrt/",basename(array_dir),".vrt")
-
-pa_vrt <- terra::vrt(array_tifs, filename = out.vrt, overwrite = TRUE)
+pa_sprc <- terra::sprc(array_tifs)
 
 out.mosaic <- paste0("/storage/home/kbl5733/gstorage/data/deepflora/maps/mosaic/",basename(array_dir),".tif")
 
-terra::mosaic(pa_vrt,
+terra::mosaic(pa_sprc,
               filename = out.mosaic,
               overwrite = TRUE
               )
