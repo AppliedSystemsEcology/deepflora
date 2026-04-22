61d067d923332a8fe610260ad6243019fe308479
Kevin <likevin@umich.edu>
2026-04-22 17:49:41 -0400

    try arrray agin

--- scripts/merge_rasters_array.R
+++ scripts/merge_rasters_array.R
@@ -1,5 +1,4 @@
 library(terra)
-terraOptions(memfrac = 0.6, tempdir = "/scratch/kbl5733/tmp")
 
 arraynum <- as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID"))
 naipdirs <- list.dirs("/storage/home/kbl5733/gstorage/data/deepflora/RASTERS/pa_250/256m_2017_-1_initial_7",
@@ -18,4 +17,16 @@ temp <- rast(naip_extent, res = res(rast(array_tifs[1])), crs = crs(naip_pa))
 # impose template raster: https://stackoverflow.com/a/72420327/12500603
 
 out.mosaic <- paste0("/storage/home/kbl5733/gstorage/data/deepflora/maps/mosaic/",basename(array_dir),".tif")
-impose(sprc(array_tifs), temp, filename = out.mosaic, overwrite = TRUE)
+
+# imposing on sprc (maybe uses more memory)
+# impose(sprc(array_tifs), temp, filename = out.mosaic, overwrite = TRUE)
+
+# Step 1: impose each tile to the template grid
+imposed_files <- vapply(array_tifs, function(f) {
+  out <- file.path("/scratch/kbl5733/tmp", basename(f))
+  impose(rast(f), temp, filename = out, overwrite = TRUE)
+  out
+}, character(1))
+
+# Step 2: mosaic the aligned tiles (no resampling needed since grids match)
+mosaic(sprc(imposed_files), filename = out.mosaic, overwrite = TRUE)
