03e8bddcd3c668b196b8ecfe909054bcbf2786f6
Kevin <likevin@umich.edu>
2026-04-25 22:11:08 -0400

    mosaic the imposed files

--- scripts/merge_rasters_array.R
+++ scripts/merge_rasters_array.R
@@ -6,21 +6,20 @@ naipdirs <- list.dirs("/storage/home/kbl5733/gstorage/data/deepflora/RASTERS/pa_
 
 array_dir <- naipdirs[arraynum]
 
+cat("Tile", array_dir)
+
+# get mosaic tifs
+array_tifs <- list.files(array_dir, pattern = "*raw.tif", full.names = TRUE)
+
+# make a template raster from extent for output
+naip_pa <-vect("/storage/home/kbl5733/work/github/deepflora/data/extents/naip_pa_albers.geojson") # get the NAIP full PA extent vector
+naip_extent <- ext(naip_pa) + 1
+temp <- rast(naip_extent, res = res(rast(array_tifs[1])), crs = crs(naip_pa))
+
+# impose template raster: https://stackoverflow.com/a/72420327/12500603
+outrast <- impose(sprc(array_tifs), temp)
+
+# write output
 out.mosaic <- paste0("/storage/home/kbl5733/gstorage/data/deepflora/maps/mosaic/",basename(array_dir),".tif")
-
-if(!file.exists(out.mosaic)){
-
-  # get mosaic tifs
-  array_tifs <- list.files(array_dir, pattern = "*raw.tif", full.names = TRUE)
-
-  # make a template raster from extent for output
-  naip_pa <-vect("/storage/home/kbl5733/work/github/deepflora/data/extents/naip_pa_albers.geojson") # get the NAIP full PA extent vector
-  naip_extent <- ext(naip_pa) + 1
-  temp <- rast(naip_extent, res = res(rast(array_tifs[1])), crs = crs(naip_pa))
-
-  # impose template raster: https://stackoverflow.com/a/72420327/12500603
-  outrast <- impose(sprc(array_tifs), temp)
-
-  writeRaster(outrast, filename = out.mosaic)
-}
-
+mosaic(outrast, fun = "mean", filename = out.mosaic, overwrite = TRUE)
+cat("Mosaicked output to", out.mosaic)
