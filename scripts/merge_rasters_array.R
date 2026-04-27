5db8011fcabe06d6a0f4116ef34b380270db91dc
Kevin <likevin@umich.edu>
2026-04-27 09:18:49 -0400

    mosaic then project

--- scripts/merge_rasters_array.R
+++ scripts/merge_rasters_array.R
@@ -6,7 +6,7 @@ naipdirs <- list.dirs("/storage/home/kbl5733/gstorage/data/deepflora/RASTERS/pa_
 
 array_dir <- naipdirs[arraynum]
 
-cat("Tile", array_dir)
+cat("Tile", basename(array_dir), "is being handled by job", Sys.getenv("SLURM_ARRAY_TASK_ID"))
 
 # get mosaic tifs
 array_tifs <- list.files(array_dir, pattern = "*raw.tif", full.names = TRUE)
@@ -16,10 +16,15 @@ naip_pa <-vect("/storage/home/kbl5733/work/github/deepflora/data/extents/naip_pa
 naip_extent <- ext(naip_pa) + 1
 temp <- rast(naip_extent, res = res(rast(array_tifs[1])), crs = crs(naip_pa))
 
-# impose template raster: https://stackoverflow.com/a/72420327/12500603
-outrast <- impose(sprc(array_tifs), temp)
-
-# write output
+# mosaic and then project
 out.mosaic <- paste0("/storage/home/kbl5733/gstorage/data/deepflora/maps/mosaic/",basename(array_dir),".tif")
-mosaic(outrast, fun = "mean", filename = out.mosaic, overwrite = TRUE)
+
+tmp_mosaic <- file.path("/scratch/kbl5733/tmp", paste0(basename(array_dir), "_tmp.tif"))
+
+cat("Mosaicking to", tmp_mosaic)
+mosaic(sprc(array_tifs), fun = "mean", filename = tmp_mosaic, overwrite = TRUE)
+
+cat("Projecting...")
+project(rast(tmp_mosaic), temp, method = "bilinear", filename = out.mosaic, overwrite = TRUE)
+
 cat("Mosaicked output to", out.mosaic)
