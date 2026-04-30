39ecd40154fcaa573362b3b51cfaae5cfa4b3cc5
Kevin <likevin@umich.edu>
2026-04-30 15:45:18 -0400

    basic partition

--- scripts/build_data_parallel.sh
+++ scripts/build_data_parallel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=df_build_data
 #SBATCH --account=open
-#SBATCH --partition=standard
+#SBATCH --partition=basic
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=28
