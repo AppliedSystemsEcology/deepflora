c081f948edf84a6347459d13921cc4f768af8f34
Kevin <likevin@umich.edu>
2026-05-02 11:07:16 -0400

    himem testing

--- scripts/run_map_terramerge_tiles_pa.sh
+++ scripts/run_map_terramerge_tiles_pa.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=merge_pa
-#SBATCH --account=open
+#SBATCH --account=hlc30_cr_default
 #SBATCH --partition=himem
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --mem=1000G
-#SBATCH --time=1:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=800G
+#SBATCH --time=72:00:00
 #SBATCH --output=mergepa_%j.out
 #SBATCH --error=mergepa_%j.err
 
