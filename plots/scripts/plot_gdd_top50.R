c517329320b975008bf9758525f79c0b62b63113
Kevin <likevin@umich.edu>
2026-05-13 14:52:28 -0400

    top 100

--- plots/scripts/plot_gdd_top50.R
+++ plots/scripts/plot_gdd_top50.R
@@ -5,31 +5,63 @@ source("R/mix_mods.R")
 
 plants_gdd <- readRDS("data/pa_floral_gdd.rds") |> sf::st_drop_geometry()
 
-# top 50
-top50 <- names(sort(table(plants_gdd$scientificName), decreasing = TRUE))[1:50]
+# all plants
+fromtop <- names(sort(table(plants_gdd$scientificName), decreasing = TRUE))
 
-mixmods50 <- list()
-for(g in seq_along(top50)){
+mixmods <- list()
+for(g in seq_along(fromtop)){
   sp.g <- plants_gdd %>%
-    dplyr::filter(scientificName == top50[g]) %>%
+    dplyr::filter(scientificName == fromtop[g]) %>%
     dplyr::filter(!is.na(gdd))
 
-  mixmods50[[top50[g]]] <- fitmix(sp.g$gdd)
+  mixmods[[fromtop[g]]] <- fitmix(sp.g$gdd)
 
 }
 
-mixdists50 <- lapply(mixmods50, make_dist_mix, 50) %>% bind_rows(.id = "scientificName")
+mixdists <- lapply(mixmods, make_dist_mix, 50) %>% bind_rows(.id = "scientificName") %>%
+  mutate(scientificName = fct_relevel(scientificName, names(mixmods)))
 
 # facet plot
-plants_top50 <- plants_gdd %>% dplyr::filter(scientificName %in% top50)
+plants_mix <- plants_gdd %>% dplyr::filter(scientificName %in% names(mixmods)) %>%
+  mutate(scientificName = fct_relevel(scientificName, names(mixmods)))
 
-mix_plot <- ggplot(plants_top50, aes(gdd)) +
+# obs count
+plants_obs_count <- table(plants_gdd$scientificName)[names(mixmods)] %>% as.data.frame() %>%
+  rename(scientificName = Var1) %>%
+  mutate(scientificName = fct_relevel(scientificName, names(mixmods)))
+
+# facet plot first 50
+
+mix_plot_50 <- ggplot(plants_mix %>% dplyr::filter(scientificName %in% names(mixmods)[1:50]), aes(gdd)) +
   geom_histogram(binwidth = 50, fill = "tan1") +
   labs(x=NULL, y="Count") + xlim(c(0,3400)) +
-  geom_line(data = mixdists50, mapping = aes(x = x, y = y, color = dist)) +
-  facet_wrap(~ scientificName, scales = "free_y", ncol = 5) +
-  scale_color_manual(values = c("red3","turquoise4"))+
+  geom_line(data = mixdists %>% dplyr::filter(scientificName %in% names(mixmods)[1:50]),
+            mapping = aes(x = x, y = y, color = dist)) +
+  geom_text(
+    data = plants_obs_count %>% dplyr::filter(scientificName %in% names(mixmods)[1:50]),
+    mapping = aes(x = Inf, y = Inf, label = Freq),
+    hjust   = 1.1,
+    vjust   = 1.2) +
+  facet_wrap(~ scientificName, scales = "free_y", ncol = 2) +
+  scale_color_manual(values = c("red3","turquoise4","burlywood4"))+
   egg::theme_article() +
   theme(legend.position = "none")
 
-ggsave("plots/mixture_top50.png", mix_plot, width = 12, height = 16, bg = "white", dpi = 300)
+# next 50
+mix_plot_100 <- ggplot(plants_mix %>% dplyr::filter(scientificName %in% names(mixmods)[51:100]), aes(gdd)) +
+  geom_histogram(binwidth = 50, fill = "tan1") +
+  labs(x=NULL, y="Count") + xlim(c(0,3400)) +
+  geom_line(data = mixdists %>% dplyr::filter(scientificName %in% names(mixmods)[51:100]),
+            mapping = aes(x = x, y = y, color = dist)) +
+  geom_text(
+    data = plants_obs_count %>% dplyr::filter(scientificName %in% names(mixmods)[51:100]),
+    mapping = aes(x = Inf, y = Inf, label = Freq),
+    hjust   = 1.1,
+    vjust   = 1.2) +
+  facet_wrap(~ scientificName, scales = "free_y", ncol = 2) +
+  scale_color_manual(values = c("red3","turquoise4","burlywood4"))+
+  egg::theme_article() +
+  theme(legend.position = "none")
+
+ggsave("plots/mixture_worked0-50.png", mix_plot_50, width = 8, height = 48, bg = "white", dpi = 300, limitsize = FALSE)
+ggsave("plots/mixture_worked50-100.png", mix_plot_100, width = 8, height = 48, bg = "white", dpi = 300, limitsize = FALSE)
