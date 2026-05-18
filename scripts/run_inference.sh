bb2ca6fc7e761c55b031ec2250656d774c3d5159
Kevin <likevin@umich.edu>
2026-05-18 14:27:26 -0400

    run inference for states

--- scripts/run_inference.sh
+++ scripts/run_inference.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=db_inf
-#SBATCH --account=hlc30_p100_default
-#SBATCH --partition=sla-prio
+#SBATCH --account=hlc30_cr_default
+#SBATCH --partition=sla-standard
 #SBATCH --gres=gpu:p100:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=26
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=224G
 #SBATCH --time=48:00:00
 #SBATCH --output=inf_%j.out
@@ -26,12 +26,17 @@ source activate deepflora
 python /storage/home/kbl5733/src/deepbiosphere/src/deepbiosphere/Inference.py \
   --band -1 \
   --model DEEPBIOSPHERE \
-  --exp_id initial \
+  --exp_id db_${STATE}_2017 \
   --loss SAMPLE_AWARE_BCE \
   --earlystopping mean_ROC_AUC \
   --batch_size 50 \
   --device 0 \
   --processes 25 \
   --year 2017 \
-  --state pa \
-  --filename initial_db
+  --state ${STATE} \
+  --filename db_${STATE}_2017
+
+if [ $? -ne 0 ]; then
+  echo "ERROR: Uniform ${STATE} deepbiosphere failed"
+fi
+
