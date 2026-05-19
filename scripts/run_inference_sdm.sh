49b38864ebdc96f2e2accccb262c80e0047bfc7e
Kevin <likevin@umich.edu>
2026-05-19 16:19:49 -0400

    run sdms

---
+++ scripts/run_inference_sdm.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH --job-name=sdm_inf
+#SBATCH --account=open
+#SBATCH --partition=basic
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=4G
+#SBATCH --time=8:00:00
+#SBATCH --output=sdm_inf_%j.out
+#SBATCH --error=sdm_inf_%j.err
+
+# $1 is the first positional argument passed to the script
+# e.g.: sbatch spatcv_inference.sh ca
+STATE=${1:?"Usage: sbatch spatcv_inference.sh <state> (e.g. ca, pa)"}
+
+if [[ ! "$STATE" =~ ^(pa|ny)$ ]]; then
+  echo "ERROR: state must be one of: pa or ny"
+  exit 1
+fi
+
+module load anaconda
+source activate deepflora
+
+python /storage/home/kbl5733/src/deepbiosphere/src/deepbiosphere/Inference.py \
+  --band -1 \
+  --model rf \
+  --dataset_name plants_${STATE}_2017 \
+  --year 2017 \
+  --state pa \
+  --filename rf_unif
+
+python /storage/home/kbl5733/src/deepbiosphere/src/deepbiosphere/Inference.py \
+  --band -1 \
+  --model maxent \
+  --dataset_name plants_pa \
+  --year 2017 \
+  --state pa \
+  --filename maxent_unif
---
+++ scripts/run_sdm.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#SBATCH --job-name=deepflora_sdm
+#SBATCH --account=open
+#SBATCH --partition=basic
+#SBATCH --gres=gpu:p100:1
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=26
+#SBATCH --mem=224G
+#SBATCH --time=8:00:00
+#SBATCH --output=sdm_rf_%j.out
+#SBATCH --error=sdm_rf_%j.err
+
+# $1 is the first positional argument passed to the script
+# e.g.: sbatch spatcv_inference.sh ca
+STATE=${1:?"Usage: sbatch spatcv_inference.sh <state> (e.g. ca, pa)"}
+
+if [[ ! "$STATE" =~ ^(pa|ny)$ ]]; then
+  echo "ERROR: state must be one of: pa or ny"
+  exit 1
+fi
+
+
+module load anaconda
+source activate deepflora_r
+
+# uniform test / train split
+echo "Processing uniform train-test random forest"
+Rscript src/deepbiosphere/src/deepbiosphere/Maxent_RF_bioclim.R \
+  --dset_name plants_${STATE}_2017 \
+  --band unif_train_test \
+  --sdm rf \
+  --ncpu 26 \
+  --remakebkgd
+
+if [ $? -ne 0 ]; then
+    echo "ERROR: uniform train-test random forest failed"
+fi
+
+echo "Processing uniform train-test maxent"
+Rscript src/deepbiosphere/src/deepbiosphere/Maxent_RF_bioclim.R \
+  --dset_name plants_${STATE}_2017 \
+  --band unif_train_test \
+  --sdm maxent \
+  --ncpu 26
+
+if [ $? -ne 0 ]; then
+    echo "ERROR: uniform train-test maxent failed"
+fi
+
+# spatial cross-validation
+for band in $(seq 0 9); do
+
+  echo "Processing band ${band} of 9 for random forest..."
+  Rscript src/deepbiosphere/src/deepbiosphere/Maxent_RF_bioclim.R \
+    --dset_name plants_${STATE}_2017 \
+    --band band_${band} \
+    --sdm rf \
+    --ncpu 26 \
+    --remakebkgd
+  if [ $? -ne 0 ]; then
+    echo "ERROR: band ${band} random forest failed"
+  fi
+
+  echo "Processing band ${band} of 9 for maxent..."
+  Rscript src/deepbiosphere/src/deepbiosphere/Maxent_RF_bioclim.R \
+    --dset_name plants_${STATE}_2017 \
+    --band band_${band} \
+    --sdm maxent \
+    --ncpu 26
+  if [ $? -ne 0 ]; then
+    echo "ERROR: band ${band} maxent failed"
+  fi
+
+done
