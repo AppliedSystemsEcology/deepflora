a61a26a07f7ac47f82d093543d139fcf09149bf8
Kevin <likevin@umich.edu>
2026-05-25 22:09:13 -0400

    code on making mosaic template

--- data-raw/naip_shp.R
+++ data-raw/naip_shp.R
@@ -29,3 +29,25 @@ library(terra)
 
 pa.v <- vect("data/extents/naip_pa_albers.geojson")
 ny.v <- vect("data/extents/naip_ny_albers.geojson")
+
+# get naip file example
+naipfiles_pa <- list.files("/storage/home/kbl5733/gstorage/data/deepflora/RASTERS/pa_250_2017/256m_2017_-1_db_pa_2017_8",
+                      recursive = TRUE, full.names = TRUE, pattern = "*raw.tif")
+pa.r <- rast(naipfiles_pa[1])
+pa.r2 <- rast(naipfiles_pa[2])
+
+naipfiles_ny <- list.files("/storage/home/kbl5733/gstorage/data/deepflora/RASTERS/ny_250_2017/256m_2017_-1_db_ny_2017_8",
+                           recursive = TRUE, full.names = TRUE, pattern = "*raw.tif")
+ny.r <- rast(naipfiles_ny[1])
+
+all.equal(res(ny.r), res(pa.r))
+
+# make the template rasters for PA and NY
+pa.albers <- project(pa.r, "epsg:5070")
+temp_pa <- rast(pa.v, res = res(pa.r), crs = crs(pa.v))
+
+ny_naip_extent <- ext(ny.v) + 1
+temp_ny <- rast(ny_naip_extent, res = res(ny.r), crs = crs(ny.v))
+
+writeRaster(temp_pa, "data/template_pa.tif")
+writeRaster(temp_ny, "data/template_ny.tif")
