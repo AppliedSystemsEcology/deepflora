306db2b1a5b840a0d95af40917f03fbaba51337c
Kevin <likevin@umich.edu>
2026-05-26 08:52:14 -0400

    loop over folders

--- scripts/project_rasters_array.r
+++ scripts/project_rasters_array.r
@@ -1,20 +1,23 @@
+start.t <- Sys.time()
 library(terra)
 
 arraynum <- as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID"))
-state <- Sys.getenv("STATE")
+args <- commandArgs(trailingOnly = TRUE)
+state <- args[1]
+
+# get folder for this array job
+arraydir <- list.dirs(
+  paste0("/storage/home/kbl5733/gstorage/data/deepflora/RASTERS/", state, "_250_2017/256m_2017_-1_db_", state, "_2017_8"),
+  full.names = TRUE
+)
+
+thisarray <- arraydir[arraynum]
 
 # get mosaic tifs
-arraytifs <- list.files(
-  paste0("/storage/home/kbl5733/gstorage/data/deepflora/RASTERS/", state, "_250_2017/256m_2017_-1_db_", state, "_2017_8"),
-           recursive = TRUE, full.names = TRUE, pattern = "*raw.tif")
 
-arraytif <- rast(arraytifs[arraynum])
+arraytifs <- list.files(thisarray, recursive = TRUE, full.names = TRUE, pattern = "*raw.tif")
 
 temp.ras <- readRDS(paste0("data/template_", state, ".rds"))
-
-cat("Projecting raster", arraynum, "for", state, "\n")
-
-projtif <- project(arraytif, temp.ras)
 
 outdir <- file.path("/storage/home/kbl5733/gstorage/data/deepflora/maps",
                     paste0(state,"_2017_albers"))
@@ -23,8 +26,27 @@ if(!exists(outdir)){
   dir.create(outdir)
 }
 
-fname <- paste0("albers_", basename(arraytifs[arraynum]))
+for(i in seq_along(arraytifs)){
+  fname <- paste0("albers_", basename(arraytifs[i]))
+  fname.outpath <- file.path(outdir, fname)
 
-writeRaster(projtif, file.path(outdir, fname))
+  if(file.exists(fname.outpath)){
+    cat("File", fname, "already exists at", outdir, "\n")
+    next
+  }
 
-cat("Tile", arraynum, "written to:", file.path(outdir, fname), "\n")
+  arraytif.i <- rast(arraytifs[i])
+
+  cat("Projecting raster", i, "of", length(arraytifs), "for", basename(thisarray), "\n")
+
+  projtif <- project(arraytif.i, temp.ras)
+
+  writeRaster(projtif, fname.outpath)
+
+  i.t <- as.numeric(Sys.time() - start.t, unit = "mins")
+
+  cat("Tile", i, "written to:", file.path(outdir, fname), "\n",
+      "It took", round(i.t, digits = 1), "minutes\n")
+
+}
+
