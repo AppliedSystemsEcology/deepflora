e69fa962837d09c843c3df52cab36b7329c076a4
Kevin <likevin@umich.edu>
2026-05-31 21:22:44 -0400

    do the rest
    script to do the rest of merge

--- scripts/run_map_merge_tiles.sh
+++ scripts/run_map_merge_tiles.sh
@@ -9,7 +9,7 @@
 #SBATCH --time=72:00:00
 #SBATCH --output=logs/mergepa_%A.out
 #SBATCH --error=logs/mergepa_%A.err
-#SBATCH --array=2
+#SBATCH --array=3-24
 
 module load anaconda
 source activate r-geo
