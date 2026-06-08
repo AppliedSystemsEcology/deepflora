a42cf4384a5c64bb635aeba492178731b2c301d4
Kevin <likevin@umich.edu>
2026-06-07 22:32:52 -0400

    update with tiles

--- scripts/check_mosaic_overlaps.py
+++ scripts/check_mosaic_overlaps.py
@@ -2,6 +2,9 @@
 from shapely.geometry import box
 import rasterio
 
+in_dir  = sys.argv[1]
+tiles = glob.glob(os.path.join(in_dir, "*.tif"))
+
 srcs = [rasterio.open(t) for t in tiles]
 boxes = [box(*src.bounds) for src in srcs]
 overlaps = sum(1 for i, a in enumerate(boxes) for b in boxes[i+1:] if a.intersects(b))
