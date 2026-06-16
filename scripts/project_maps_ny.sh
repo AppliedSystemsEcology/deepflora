00037e98eaa44cb02b35c51e98ea5a28d5456f6b
Kevin Li <kbl5733@submit04.2e.hpc.psu.edu>
2026-06-16 16:13:49 -0400

    test project ny

--- scripts/project_maps_ny.sh
+++ scripts/project_maps_ny.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=proj_ny
 #SBATCH --account=hlc30_cr_default
-#SBATCH --partition=basic
+#SBATCH --partition=himem
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=64G
 #SBATCH --time=48:00:00
 #SBATCH --output=logs/prjny_%A.out
-#SBATCH --error=logs/prjny_%A_%a.err
-#SBATCH --array=1-32
+#SBATCH --error=logs/prjny_%A.err
+#SBATCH --array=1
 
 module load anaconda
 source activate r-geo
