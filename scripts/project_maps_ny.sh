eb5da7405a74f5948f6854ad4fcacf23378fd4d0
Kevin <likevin@umich.edu>
2026-07-15 01:13:56 -0400

    cherry pick the missing tiles to project

--- scripts/project_maps_ny.sh
+++ scripts/project_maps_ny.sh
@@ -9,7 +9,7 @@
 #SBATCH --time=48:00:00
 #SBATCH --output=logs/prjny_%A.out
 #SBATCH --error=logs/prjny_%A.err
-#SBATCH --array=1-32
+#SBATCH --array=1,17
 
 module load anaconda
 source activate r-geo
