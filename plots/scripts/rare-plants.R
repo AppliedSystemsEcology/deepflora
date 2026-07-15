7c12e2f23ee47271e7bd5aeb1043686fa3dfc092
Kevin <likevin@umich.edu>
2026-07-15 15:23:10 -0400

    mosaic_big_tiles

--- plots/scripts/rare-plants.R
+++ plots/scripts/rare-plants.R
@@ -1,4 +1,6 @@
 library(terra)
+library(tidyverse)
+library(tidyterra)
 
 plants <- read.csv("data-raw/rare-plants.csv")
 
@@ -13,7 +15,25 @@ plants$species[!(plants$species %in% names(dfpa))]   # Solidago speciosa is miss
 plants$species[plants$species %in% names(mixm)]
 
 for(i in seq_len(nrow(plants_in))){
-  terra::plot(dfpa[[plants_in$species[i]]])
+
+  plot_i <- ggplot() + geom_spatraster(data=dfpa[[plants_in$species[i]]]) +
+    scale_fill_viridis_c(
+      option = "rocket",
+      na.value = "transparent"
+    ) +
+    theme_bw() +
+    theme(
+      axis.text = element_blank(),
+      axis.ticks = element_blank()
+    ) +
+    labs(
+      fill = "",
+      title = plants_in$species[i]
+    )
+
+  ggsave(paste0("plots/rare_plants/",gsub(" ", "_", plants_in$species[i]),".png"),
+         plot_i, width = 4, height = 2.5, units = "in", bg = "white", dpi = 300)
+
 }
 
 
--- scripts/mosaic_big_tiles.sh
+++ scripts/mosaic_big_tiles.sh
@@ -7,12 +7,12 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16G
 #SBATCH --time=04:00:00
-#SBATCH --output=logs/pymerge_pa_%A.out
-#SBATCH --error=logs/pymerge_pa_%A.err
+#SBATCH --output=logs/pymerge_ny_%A.out
+#SBATCH --error=logs/pymerge_ny_%A.err
 
 module load anaconda
 source activate py-geo
 
 python /storage/home/kbl5733/work/github/deepflora/scripts/mosaic_big_tiles.py \
-  /storage/home/kbl5733/gstorage/data/deepflora/maps/pa_2017_merge_block \
+  /storage/home/kbl5733/gstorage/data/deepflora/maps/ny_2017_merge_block \
   /storage/home/kbl5733/gstorage/data/deepflora/maps/out
