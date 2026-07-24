1c1f5cd5949e644e97e3da67534959bbf899ef19
Kevin <likevin@umich.edu>
2026-07-24 17:22:39 -0400

    test raster prediction

--- scripts/run_raster_prediction.sh
+++ scripts/run_raster_prediction.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=deepflora_prediction
-#SBATCH --account=hlc30_p100_default
+#SBATCH --account=hlc30_cr_default
 #SBATCH --partition=sla-prio
 #SBATCH --gres=gpu:p100:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=26
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=224G
 #SBATCH --time=48:00:00
 #SBATCH --output=prediction_%j.out
@@ -14,4 +14,4 @@
 module load anaconda
 source activate deepflora
 
-python work/github/deepflora/scripts/predict_State_College.py
+python work/github/deepflora/scripts/predict_raster.py
