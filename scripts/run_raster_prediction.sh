e0833d695d67f04047a9454ec409598d1e71d3ae
Kevin <likevin@umich.edu>
2026-07-24 17:47:53 -0400

    correct file paths

--- scripts/run_raster_prediction.sh
+++ scripts/run_raster_prediction.sh
@@ -8,10 +8,10 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=224G
 #SBATCH --time=48:00:00
-#SBATCH --output=prediction_%j.out
-#SBATCH --error=prediction_%j.err
+#SBATCH --output=logs/prediction_%j.out
+#SBATCH --error=logs/prediction_%j.err
 
 module load anaconda
 source activate deepflora
 
-python work/github/deepflora/scripts/predict_raster.py
+python scripts/predict_raster.py
