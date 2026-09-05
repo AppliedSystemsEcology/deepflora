11b933acaea3d0a4190fc68ab137b690159703eb
Kevin <likevin@umich.edu>
2026-09-05 16:10:04 -0400

    fix counting

--- scripts/run_raster_prediction.sh
+++ scripts/run_raster_prediction.sh
@@ -17,7 +17,7 @@ source activate deepflora
 mapfile -t files < scripts/aaron/filenames.txt
 total=${#files[@]}
 
-for i in "${files[@]}"
+for i in "${!files[@]}"
 do
   echo "[$(date '+%Y-%m-%d %H:%M:%S')] Processing file $((i+1)) of $total: ${files[$i]}"
   python scripts/predict_raster.py "$i" ny
