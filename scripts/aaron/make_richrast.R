129f3e6dca2bca127995981117197c775489e707
Kevin <likevin@umich.edu>
2026-09-14 15:49:21 -0400

    make richness

---
+++ scripts/aaron/make_richrast.R
@@ -0,0 +1,23 @@
+library(terra)
+library(sf)
+library(tidyverse)
+
+nydir <- "/storage/home/kbl5733/gstorage/data/deepflora/maps/tiles"
+pts <- st_read("scripts/aaron/iverson_pts_naipjoin.geojson")
+
+tiles <- unique(pts$FileName)
+
+outlist <- list()
+
+for(i in seq_along(tiles)){
+
+  tile.i <- rast(file.path(nydir,paste0("ny_30m_",tiles[i],"_raw.tif")))
+
+  # map richness
+
+  pres.i <- tile.i > 0.5
+  rich.i <- sum(pres.i)
+
+  writeRaster(rich.i, file.path("data/aaron",paste0("rich_",tile.i)))
+
+}
